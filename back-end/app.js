011a6816532a9a0896303618faf79c4e75f8216d
ngoclias <ngoc.leanh1999@gmail.com>
2022-09-09 07:41:27 +0700

    add uploaded image

--- back-end/app.js
+++ back-end/app.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -10,41 +13,43 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
-  next();
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+    next();
 });
 
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
 app.use((res, req, next) => {
-  const error = new HttpError('Could not find this route.', 404);
-  throw error;
+    const error = new HttpError('Could not find this route.', 404);
+    throw error;
 });
 
 app.use((error, req, res, next) => {
-  if (res.headerSent) {
-    return next(error);
-  }
+    if (req.file) {
+        fs.unlink(req.file.path, (err) => {
+            console.log(err);
+        });
+    }
+    if (res.headerSent) {
+        return next(error);
+    }
 
-  res.status(error.code || 500);
-  res.json({ message: error.message || 'An unknown error occurred!' });
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
 mongoose
-  .connect(
-    'mongodb+srv://nemo:Ngoclias213@cluster0.b49fq.mongodb.net/mern?retryWrites=true&w=majority'
-  )
-  .then(() => {
-    app.listen(5000);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect('mongodb+srv://nemo:Ngoclias213@cluster0.b49fq.mongodb.net/mern?retryWrites=true&w=majority')
+    .then(() => {
+        app.listen(5000);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
--- back-end/controllers/users-controllers.js
+++ back-end/controllers/users-controllers.js
@@ -4,100 +4,78 @@ const HttpError = require('../models/http-error');
 const User = require('../models/user');
 
 const getUsers = async (req, res, next) => {
-  let users;
-  try {
-    users = await User.find({}, '-password');
-  } catch (err) {
-    const error = new HttpError(
-      'Fetching users failed, please try again later.',
-      500
-    );
-    return next(error);
-  }
+    let users;
+    try {
+        users = await User.find({}, '-password');
+    } catch (err) {
+        const error = new HttpError('Fetching users failed, please try again later.', 500);
+        return next(error);
+    }
 
-  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
+    res.json({ users: users.map((user) => user.toObject({ getters: true })) });
 };
 
 const signup = async (req, res, next) => {
-  const errors = validationResult(req);
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    const error = new HttpError(
-      'Invalid inputs passes, please check your data.',
-      422
-    );
-    return next(error);
-  }
+    if (!errors.isEmpty()) {
+        const error = new HttpError('Invalid inputs passes, please check your data.', 422);
+        return next(error);
+    }
 
-  const { name, email, password } = req.body;
+    const { name, email, password } = req.body;
 
-  let existingUser;
-  try {
-    existingUser = await User.findOne({ email: email });
-  } catch (err) {
-    const error = new HttpError(
-      'Signing up failed, please try again later.',
-      500
-    );
-    return next(error);
-  }
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email: email });
+    } catch (err) {
+        const error = new HttpError('Signing up failed, please try again later.', 500);
+        return next(error);
+    }
 
-  if (existingUser) {
-    const error = new HttpError(
-      'User exists already, please login instead.',
-      422
-    );
-    return next(error);
-  }
+    if (existingUser) {
+        const error = new HttpError('User exists already, please login instead.', 422);
+        return next(error);
+    }
 
-  const createUser = new User({
-    name,
-    email,
-    password,
-    image:
-      'https://images.unsplash.com/photo-1470093851219-69951fcbb533?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y3V0ZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-    places: [],
-  });
+    const createUser = new User({
+        name,
+        email,
+        password,
+        image: req.file.path,
+        places: [],
+    });
 
-  try {
-    await createUser.save();
-  } catch (err) {
-    const error = new HttpError(
-      'Signing up failed, please try again later.',
-      500
-    );
-    return next(error);
-  }
+    try {
+        await createUser.save();
+    } catch (err) {
+        const error = new HttpError('Signing up failed, please try again later.', 500);
+        return next(error);
+    }
 
-  res.status(201).json({ user: createUser.toObject({ getters: true }) });
+    res.status(201).json({ user: createUser.toObject({ getters: true }) });
 };
 
 const login = async (req, res, next) => {
-  const { email, password } = req.body;
+    const { email, password } = req.body;
 
-  let existingUser;
-  try {
-    existingUser = await User.findOne({ email: email });
-  } catch (err) {
-    const error = new HttpError(
-      'Logging in failed, please try again later.',
-      500
-    );
-    return next(error);
-  }
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email: email });
+    } catch (err) {
+        const error = new HttpError('Logging in failed, please try again later.', 500);
+        return next(error);
+    }
 
-  if (!existingUser || existingUser.password !== password) {
-    const error = new HttpError(
-      'Invalid credentials, could not log you in.',
-      401
-    );
-    return next(error);
-  }
+    if (!existingUser || existingUser.password !== password) {
+        const error = new HttpError('Invalid credentials, could not log you in.', 401);
+        return next(error);
+    }
 
-  res.json({
-    message: 'Logged in!',
-    user: existingUser.toObject({ getters: true }),
-  });
+    res.json({
+        message: 'Logged in!',
+        user: existingUser.toObject({ getters: true }),
+    });
 };
 
 exports.getUsers = getUsers;
